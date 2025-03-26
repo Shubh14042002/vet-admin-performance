61790b555e3c927fa584a81e95c3441c479acfef
Sayma Haque <sayma.haque@ucalgary.ca>
2025-03-25 22:03:11 -0600

    update test to correct port

--- tests/load_test.js
+++ tests/load_test.js
@@ -8,18 +8,18 @@ export let options = {
 
 export default function () {
    // Fetch Patients
-   let res = http.get("http://localhost:3000/patients");
+   let res = http.get("http://localhost:3001/patients");
    check(res, { "GET /patients status was 200": (r) => r.status === 200 });
 
    // Add a New Patient
    let payload = JSON.stringify({ name: "Max", age: 4, species: "Dog", owner: "John" });
    let params = { headers: { "Content-Type": "application/json" } };
-   let postRes = http.post("http://localhost:3000/patients", payload, params);
+   let postRes = http.post("http://localhost:3001/patients", payload, params);
    check(postRes, { "POST /patients status was 201": (r) => r.status === 201 });
 
    // Delete the Newly Added Patient
    let patientId = JSON.parse(postRes.body).id;  // Get new patient ID
-   let deleteRes = http.del(`http://localhost:3000/patients/${patientId}`);
+   let deleteRes = http.del(`http://localhost:3001/patients/${patientId}`);
    check(deleteRes, { "DELETE /patients status was 200": (r) => r.status === 200 });
 
    sleep(1); // Wait between requests
